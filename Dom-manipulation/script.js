2e55d62404dbc79eab7c7932e512757888eb8cb8
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-16 22:34:54 +0100

    Done with the part of creating html elements with js

--- Dom-manipulation/script.js
+++ Dom-manipulation/script.js
@@ -9,3 +9,15 @@ const   h3 = document.createElement("h3");
 h3.textContent = "I'm a blue h3!";
 h3.style.color = "blue";
 container.appendChild(h3);
+
+const div = document.createElement("div");
+div.setAttribute("style", "background: purple; height: 300px; width: 300px; border: 8px solid black");
+container.appendChild(div);
+
+const   h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+const   secondParagraph = document.createElement("p");
+secondParagraph.textContent = "ME TOO!";
+div.appendChild(secondParagraph);
